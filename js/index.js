5e864719ab221ad7e54192d640ad475aea35810c
lantip <lantip@gmail.com>
2022-01-04 21:52:18 +0700

    fixing tooltip. adding: rikma, janggut, pilingan, pipi

--- js/index.js
+++ js/index.js
@@ -10,26 +10,13 @@ $(document).ready(function() {
         fillColor: "d42e16",
         strokeColor: "3320FF",
         strokeOpacity: 0.8,
-        strokeWidth: 4,
+        strokeWidth: 1,
         stroke: true,
         isSelectable: true,
-        singleSelect: false,
+        singleSelect: true,
         mapKey: 'alt',
         listKey: 'key',
-        onClick: function(e) {
-            var newToolTip = defaultDipTooltip;
-            if ($.inArray(e.key, selectedItems) >= 0) {
-                selectedItems.splice($.inArray(e.key, selectedItems), 1);
-            } else {
-                selectedItems.push(e.key);
-            }
-            // $('#selections').html( selectedItems.length);
-            //$('#selections').html(selectedItems.length > 0 ? "<b>Selected body Parts: </b>" + selectedItems.toString().replace(new RegExp('_', 'g'), " ").replace(new RegExp(',', 'g'), ", ") : "<b>Please select a body part</b>");
-
-        },
-        showToolTip: true,
-        toolTipClose: ["tooltip-click", "area-click"],
-        areas: [{}, ]
+        showToolTip: false
     });
     $('.tooltip').tooltipster({
         delay: 100,
